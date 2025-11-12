4a7199d0930cc3e8883f92ba7a6edb6299f8cf10
Andrii Kachmar <Andrii.Kachmar.KN.2022@lpnu.ua>
2025-11-12 17:18:35 +0200

    just to show conflict

---
+++ src/main/java/family_tree/controller/PersonalInformationController.java
@@ -0,0 +1,16 @@
+package family_tree.controller;
+
+import family_tree.service.PersonalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class PersonalInformationController {
+
+    private final PersonalService personalService;
+
+
+
+
+}
